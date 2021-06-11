4c4c74d07c4792e8f60b4179db3979873591a2a6
BEACON <1257839167@qq.com>
2021-06-11 16:20:12 +0800

    initialize FirstApply

---
+++ src/main/java/com/example/mybatisplus/model/domain/FirstApply.java
@@ -0,0 +1,64 @@
+package com.example.mybatisplus.model.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Beacon
+ * @since 2021-06-11
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="FirstApply对象", description="")
+public class FirstApply extends Model<FirstApply> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "fa_id", type = IdType.AUTO)
+    private Long faId;
+
+    private Long sId;
+
+    private Integer tpId;
+
+    private Integer score;
+
+    private String name;
+
+    private String phone;
+
+    private String address;
+
+    private String emergencyPhone;
+
+    private String physicalIllness;
+
+    private Boolean isDiagnosed;
+
+    private String emergencyLevel;
+
+    private String problemType;
+
+    private String consultExpectation;
+
+    private String consultHistory;
+
+
+    @Override
+    protected Serializable pkVal() {
+        return this.faId;
+    }
+
+}
