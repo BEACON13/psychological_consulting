8639b08ded2da6a0a222436e56c311e43c2e8169
mouao <416633359@qq.com>
2021-06-13 14:43:10 +0800

    Session

--- src/main/java/com/example/mybatisplus/common/utls/SecurityUtils.java
+++ src/main/java/com/example/mybatisplus/common/utls/SecurityUtils.java
@@ -1,6 +1,6 @@
 package com.example.mybatisplus.common.utls;
 
-import com.example.mybatisplus.model.domain.Admin;
+import com.example.mybatisplus.model.domain.Person;
 import com.example.mybatisplus.model.dto.UserInfoDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +14,7 @@ public class SecurityUtils {
      *
      * @return
      */
+    /*
     public static Admin getCurrentUserInfo() {
         Admin userInfo = SessionUtils.getCurrentUserInfo();
         //模拟登录
@@ -24,7 +25,6 @@ public class SecurityUtils {
 
         return userInfo;
     }
-
     public static UserInfoDTO getUserInfo() {
         Admin userInfo = SessionUtils.getCurrentUserInfo();
         UserInfoDTO userInfoDTO = new UserInfoDTO();
@@ -39,6 +39,36 @@ public class SecurityUtils {
             userInfoDTO.setId(1L);
             userInfoDTO.setName("模拟用户");
             userInfoDTO.setUserType(1L);
+        }
+
+        return userInfoDTO;
+    }
+     */
+
+    public static Person getCurrentUserInfo() {
+        Person userInfo = SessionUtils.getCurrentUserInfo();
+        //模拟登录
+        if (userInfo == null) {
+            userInfo = new Person();
+            userInfo.setUsername("模拟");
+        }
+
+        return userInfo;
+    }
+
+    public static UserInfoDTO getUserInfo() {
+        Person userInfo = SessionUtils.getCurrentUserInfo();
+        UserInfoDTO userInfoDTO = new UserInfoDTO();
+        //模拟登录
+        if (userInfo == null) {
+            userInfo = new Person();
+            userInfo.setUsername("模拟用户");
+            userInfoDTO.setId(1L);
+            userInfoDTO.setUsername("模拟用户");
+        }else{
+            userInfoDTO.setId(userInfo.getPId());
+            userInfoDTO.setUsername(userInfo.getUsername());
+            userInfoDTO.setPwd(userInfo.getPassword());
         }
 
         return userInfoDTO;
