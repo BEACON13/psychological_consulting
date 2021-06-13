796e2b71a625f5143c8084b4492bcbd85640eae1
BEACON <1257839167@qq.com>
2021-06-13 16:09:24 +0800

    initialize ConsultAppointmentRecord

---
+++ src/main/java/com/example/mybatisplus/mapper/ConsultAppointmentRecordMapper.java
@@ -0,0 +1,16 @@
+package com.example.mybatisplus.mapper;
+
+import com.example.mybatisplus.model.domain.ConsultAppointmentRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Beacon
+ * @since 2021-06-13
+ */
+public interface ConsultAppointmentRecordMapper extends BaseMapper<ConsultAppointmentRecord> {
+
+}
---
+++ src/main/java/com/example/mybatisplus/service/ConsultAppointmentRecordService.java
@@ -0,0 +1,16 @@
+package com.example.mybatisplus.service;
+
+import com.example.mybatisplus.model.domain.ConsultAppointmentRecord;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Beacon
+ * @since 2021-06-13
+ */
+public interface ConsultAppointmentRecordService extends IService<ConsultAppointmentRecord> {
+
+}
