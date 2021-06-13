312cb2b36d5e021aadd83f34285914a987874743
BEACON <1257839167@qq.com>
2021-06-13 11:17:21 +0800

    feat:student login

--- src/main/java/com/example/mybatisplus/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/mybatisplus/service/impl/StudentServiceImpl.java
@@ -1,11 +1,16 @@
 package com.example.mybatisplus.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.mybatisplus.model.domain.Student;
 import com.example.mybatisplus.mapper.StudentMapper;
 import com.example.mybatisplus.service.StudentService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.xml.ws.wsaddressing.W3CEndpointReference;
+import java.sql.Wrapper;
+import java.util.List;
 
 /**
  * <p>
@@ -23,7 +28,11 @@ public class StudentServiceImpl extends ServiceImpl<StudentMapper, Student> impl
 
     @Override
     public Student login(String code){
-        Student student=studentMapper.login(code);
-        return student;
+        //Student student=studentMapper.login(code);
+        //return student;
+        QueryWrapper wrapper=new QueryWrapper<Student>();
+        wrapper.eq("code","2018141411216");
+        List<Student> studentList = studentMapper.selectList(wrapper);
+        return studentList.get(0);
     }
 }
