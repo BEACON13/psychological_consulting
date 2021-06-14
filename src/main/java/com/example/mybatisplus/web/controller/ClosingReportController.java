1d724fc2a9c467a04c47f59fe006e3f23fc67897
BEACON <1257839167@qq.com>
2021-06-14 10:53:48 +0800

    feat:create closing report

--- src/main/java/com/example/mybatisplus/web/controller/ClosingReportController.java
+++ src/main/java/com/example/mybatisplus/web/controller/ClosingReportController.java
@@ -1,5 +1,6 @@
 package com.example.mybatisplus.web.controller;
 
+import com.example.mybatisplus.service.ConsultAppointmentRecordService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.slf4j.Logger;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import com.example.mybatisplus.common.JsonResponse;
 import com.example.mybatisplus.service.ClosingReportService;
 import com.example.mybatisplus.model.domain.ClosingReport;
+
+import java.util.Map;
 
 
 /**
@@ -29,6 +32,29 @@ public class ClosingReportController {
     @Autowired
     private ClosingReportService closingReportService;
 
+    @Autowired
+    private ConsultAppointmentRecordService consultAppointmentRecordService;
+    /*
+    插入
+     */
+    @RequestMapping(value = "/insert", method = RequestMethod.GET)
+    @ResponseBody
+    public JsonResponse insertClosingReport(@RequestParam("closingReport") Map<String,Object> info) throws Exception {
+        ClosingReport report = new ClosingReport();
+        report.setCId((Long) info.get("cId"))
+                .setConsultEffectSelf((String) info.get("consultEffectSelf"))
+                .setProblemType((String) info.get("problemType"))
+                .setSId((Long) info.get("sId"));
+
+        int num = consultAppointmentRecordService
+                .countConsultingNum((Long) info.get("sId"));
+        report.setConsultNum(num);
+
+        consultAppointmentRecordService
+                .finishAppointment((Long) info.get("consultAppointId"));
+
+        return create(report);
+    }
     /**
     * 描述：根据Id 查询
     *
