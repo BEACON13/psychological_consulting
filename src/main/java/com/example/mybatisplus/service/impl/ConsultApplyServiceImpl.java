37876d300b2da81c0ad6d456394cb2af397ccb03
BEACON <1257839167@qq.com>
2021-06-14 20:12:16 +0800

    initialize ConsultApply

---
+++ src/main/java/com/example/mybatisplus/service/impl/ConsultApplyServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.mybatisplus.service.impl;
+
+import com.example.mybatisplus.model.domain.ConsultApply;
+import com.example.mybatisplus.mapper.ConsultApplyMapper;
+import com.example.mybatisplus.service.ConsultApplyService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Beacon
+ * @since 2021-06-14
+ */
+@Service
+public class ConsultApplyServiceImpl extends ServiceImpl<ConsultApplyMapper, ConsultApply> implements ConsultApplyService {
+
+}
