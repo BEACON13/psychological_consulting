d2293ac741b199a7cf519e1c2e59131c7cdd0f87
mouao <416633359@qq.com>
2021-06-14 17:33:11 +0800

    fvrecord

--- src/main/java/com/example/mybatisplus/mapper/FirstVisitRecordMapper.java
+++ src/main/java/com/example/mybatisplus/mapper/FirstVisitRecordMapper.java
@@ -2,7 +2,10 @@ package com.example.mybatisplus.mapper;
 
 import com.example.mybatisplus.model.domain.FirstVisitRecord;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.mybatisplus.model.vo.FirstVisitRecordVO;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,5 +17,10 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface FirstVisitRecordMapper extends BaseMapper<FirstVisitRecord> {
+    List<FirstVisitRecordVO> showRecords(Long id);
+
+    List<FirstVisitRecordVO> showAllRecords(Long id);
+
+    List<FirstVisitRecordVO> getRecordsByName(Long id, String stuName);
 
 }
--- src/main/java/com/example/mybatisplus/service/impl/FirstVisitRecordServiceImpl.java
+++ src/main/java/com/example/mybatisplus/service/impl/FirstVisitRecordServiceImpl.java
@@ -3,8 +3,10 @@ package com.example.mybatisplus.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.mybatisplus.common.JsonResponse;
 import com.example.mybatisplus.common.utls.SecurityUtils;
+import com.example.mybatisplus.mapper.StudentMapper;
 import com.example.mybatisplus.model.domain.FirstVisitRecord;
 import com.example.mybatisplus.mapper.FirstVisitRecordMapper;
+import com.example.mybatisplus.model.vo.FirstVisitRecordVO;
 import com.example.mybatisplus.service.FirstVisitRecordService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,27 +33,64 @@ public class FirstVisitRecordServiceImpl extends ServiceImpl<FirstVisitRecordMap
     @Autowired
     FirstVisitRecordMapper firstVisitRecordMapper;
 
+
+
+    /**
+     * 描述：显示该初访员所有未完成的预约
+     *
+     */
     @Override
-    public JsonResponse showRecord() {
-
+    public JsonResponse showRecords() {
+        //搜索未完成的预约
         Long id = SecurityUtils.getUserInfo().getId();
-        QueryWrapper<FirstVisitRecord> wrapper = new QueryWrapper<>();
-        wrapper.lambda().eq(FirstVisitRecord::getFvId,id).eq(FirstVisitRecord::getIsFinished,0);
-        List<FirstVisitRecord> firstVisitRecords = firstVisitRecordMapper.selectList(wrapper);
+        List<FirstVisitRecordVO> firstVisitRecordVOS = firstVisitRecordMapper.showRecords(id);
 
-
+        //搜索未填报的预约
         LocalDate date = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         date.format(formatter);
-        QueryWrapper<FirstVisitRecord> wrapper2 = new QueryWrapper<>();
-        wrapper2.lambda().eq(FirstVisitRecord::getFvId,id).le(FirstVisitRecord::getDate,date).eq(FirstVisitRecord::getIsFinished,0);
-        List<FirstVisitRecord> firstVisitRecords2 = firstVisitRecordMapper.selectList(wrapper2);
+        QueryWrapper<FirstVisitRecord> wrapper = new QueryWrapper<>();
+        wrapper.lambda().eq(FirstVisitRecord::getFvId,id).le(FirstVisitRecord::getDate,date).eq(FirstVisitRecord::getIsFinished,0);
+        List<FirstVisitRecord> firstVisitRecords = firstVisitRecordMapper.selectList(wrapper);
+        List<Long> ids = new ArrayList<>();
+        for ( FirstVisitRecord firstVisitRecord : firstVisitRecords
+             ) {
+            ids.add(firstVisitRecord.getFvrId());
+        }
 
-        System.out.println(firstVisitRecords);
-        System.out.println(firstVisitRecords2);
+        //System.out.println(firstVisitRecordVOS);
+        //System.out.println(ids);
         Map<String,List> map = new HashMap();
-        map.put("未完成",firstVisitRecords);
-        map.put("未填报",firstVisitRecords2);
-        return JsonResponse.success(map,"完成！");
+        map.put("未完成",firstVisitRecordVOS);
+        map.put("未填报",ids);
+
+        return JsonResponse.success(map,"success!");
+
     }
+
+    /**
+     * 描述：显示该初访员所有预约
+     *
+     */
+    @Override
+    public JsonResponse showAllRecords() {
+        Long id = SecurityUtils.getUserInfo().getId();
+        List<FirstVisitRecordVO> firstVisitRecordVOS = firstVisitRecordMapper.showAllRecords(id);
+
+        System.out.println(firstVisitRecordVOS);
+        return JsonResponse.success(firstVisitRecordVOS,"success!");
+    }
+
+    /**
+     * 描述：该初访员根据学生姓名搜索是否有属于自己的初访记录
+     *
+     */
+    @Override
+    public JsonResponse getRecordsByName(String stuName) {
+        Long id = SecurityUtils.getUserInfo().getId();
+        List<FirstVisitRecordVO> firstVisitRecordVOS = firstVisitRecordMapper.getRecordsByName(id,stuName);
+        return JsonResponse.success(firstVisitRecordVOS,"success!");
+    }
+
+
 }
