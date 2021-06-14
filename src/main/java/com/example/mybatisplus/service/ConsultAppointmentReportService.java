944af2bd02ce86b83a48aa2e5aa512fbe3ee814a
BEACON <1257839167@qq.com>
2021-06-14 12:43:32 +0800

    initialize ConsultAppointmentReport

---
+++ src/main/java/com/example/mybatisplus/service/ConsultAppointmentReportService.java
@@ -0,0 +1,16 @@
+package com.example.mybatisplus.service;
+
+import com.example.mybatisplus.model.domain.ConsultAppointmentReport;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Kristy
+ * @since 2021-06-14
+ */
+public interface ConsultAppointmentReportService extends IService<ConsultAppointmentReport> {
+
+}
