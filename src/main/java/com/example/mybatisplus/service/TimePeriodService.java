236b99032fc6a35bb68973c5e4aaecadc596cc71
BEACON <1257839167@qq.com>
2021-06-15 11:18:19 +0800

    initialize TimePeriod

---
+++ src/main/java/com/example/mybatisplus/service/TimePeriodService.java
@@ -0,0 +1,16 @@
+package com.example.mybatisplus.service;
+
+import com.example.mybatisplus.model.domain.TimePeriod;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Beacon
+ * @since 2021-06-15
+ */
+public interface TimePeriodService extends IService<TimePeriod> {
+
+}
