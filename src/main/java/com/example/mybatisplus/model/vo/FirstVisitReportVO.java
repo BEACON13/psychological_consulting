5ccfdac974e72f8b3141e7351ffcb4c34f8c29f1
mouao <416633359@qq.com>
2021-06-15 11:16:39 +0800

    new

--- src/main/java/com/example/mybatisplus/model/vo/FirstVisitReportVO.java
+++ src/main/java/com/example/mybatisplus/model/vo/FirstVisitReportVO.java
@@ -1,10 +1,31 @@
 package com.example.mybatisplus.model.vo;
 
 import com.example.mybatisplus.model.domain.FirstVisitReport;
+import lombok.Data;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class FirstVisitReportVO extends FirstVisitReport {
+@Data
+public class FirstVisitReportVO {
+
+    private Long fvreportId;
+
+    private Long fvrId;
+
+    private Long sId;
+
+    private Integer tpId;
+
+    private Long fvId;
+
+    private String dangerLevel;
+
+    private String problemType;
+
+    private String conclusion;
+
+    private LocalDate date;
 
     private String stuname;
 
